17f048daa64a6b82f39f8a48e967459b07126561
pinetree <eodnjs568@gmail.com>
2024-05-14 18:43:15 +0900

    Refactor : FileStore 패키지 위치 및 상품이미지 등록 @RequestPart 적용

--- shoppingmall/src/main/java/shppingmall/commerce/common/FileStore.java
+++ shoppingmall/src/main/java/shppingmall/commerce/common/FileStore.java
@@ -1,4 +1,4 @@
-package shppingmall.commerce.config;
+package shppingmall.commerce.common;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
