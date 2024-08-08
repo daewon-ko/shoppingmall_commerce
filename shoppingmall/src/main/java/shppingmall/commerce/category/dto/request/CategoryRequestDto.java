0f82c69197d1bd340c51fec9cc7ff11e2c498b70
pinetree <eodnjs568@gmail.com>
2024-08-08 16:26:19 +0900

    Test : CategoryService 테스트 작성
    - 카테고리 생성요청에 따라 이름 지정 후 카테고리 생성

--- shoppingmall/src/main/java/shppingmall/commerce/category/dto/request/CategoryRequestDto.java
+++ shoppingmall/src/main/java/shppingmall/commerce/category/dto/request/CategoryRequestDto.java
@@ -1,17 +1,20 @@
 package shppingmall.commerce.category.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.validator.constraints.Range;
 import shppingmall.commerce.category.entity.Category;
 
-import java.time.LocalDateTime;
 
 @Getter
 public class CategoryRequestDto {
     @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String categoryName;
+
+    @Builder
+    private CategoryRequestDto(String categoryName) {
+        this.categoryName = categoryName;
+    }
 
     public Category toEntity() {
         return Category.builder()
---
+++ shoppingmall/src/test/java/shppingmall/commerce/category/service/CategoryServiceTest.java
@@ -0,0 +1,36 @@
+package shppingmall.commerce.category.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import shppingmall.commerce.category.dto.request.CategoryRequestDto;
+import shppingmall.commerce.category.repository.CategoryRepository;
+import shppingmall.commerce.support.IntegrationTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CategoryServiceTest extends IntegrationTest {
+
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @DisplayName("카테고리 이름을 받아서 카테고리를 생성한다. ")
+    @Test
+    void createCategory() {
+        //given
+        CategoryRequestDto request = CategoryRequestDto.builder()
+                .categoryName("카테고리")
+                .build();
+        //when
+        long savedCategoryId = categoryService.createCategory(request);
+
+        //then
+        assertThat(categoryRepository.findByName(request.getCategoryName()).getId())
+                .isEqualTo(savedCategoryId);
+
+    }
+
+
+}
