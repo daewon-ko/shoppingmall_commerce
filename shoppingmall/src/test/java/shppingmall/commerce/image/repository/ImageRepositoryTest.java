784ecfb826d12f6aeab3eee4e116eace76b6fd98
pinetree <eodnjs568@gmail.com>
2024-08-27 00:32:44 +0900

    Test : ImageRepository, productId및 FileType으로 조회하는 테스트 코드 작성

--- shoppingmall/src/test/java/shppingmall/commerce/image/repository/ImageRepositoryTest.java
+++ shoppingmall/src/test/java/shppingmall/commerce/image/repository/ImageRepositoryTest.java
@@ -1,11 +1,14 @@
 package shppingmall.commerce.image.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 import shppingmall.commerce.image.entity.FileType;
 import shppingmall.commerce.image.entity.Image;
 import shppingmall.commerce.product.entity.Product;
@@ -25,11 +28,15 @@ class ImageRepositoryTest {
 
     @Autowired
     private ProductRepository productRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     @AfterEach
     void tearDown() {
         imageRepository.deleteAllInBatch();
         productRepository.deleteAllInBatch();
+//        em.createNativeQuery("ALTER table image AUTO_INCREMENT = 1").executeUpdate();
+
     }
 
     @DisplayName("상품에 저장되어 있는 단일 이미지를 조회할 수 있다..")
@@ -42,15 +49,15 @@ class ImageRepositoryTest {
         Image image = createImage(1L, "upload", savedProduct);
 
 
-        imageRepository.save(image);
+        Image savedImage = imageRepository.save(image);
 
 
         //when
-        List<Image> images = productRepository.findImagesByProductId(FileType.PRODUCT_IMAGE, savedProduct.getId());
+        List<Image> images = imageRepository.findImagesByTargetIdAndFileType(FileType.PRODUCT_IMAGE, savedProduct.getId());
 
         //then
         assertThat(images.get(0)).extracting(Image::getId, Image::getFileType, Image::getUploadName)
-                .contains(1L, FileType.PRODUCT_IMAGE, "upload");
+                .contains(savedImage.getId(), FileType.PRODUCT_IMAGE, "upload");
 
     }
 
@@ -68,18 +75,21 @@ class ImageRepositoryTest {
         Image image2 = createImage(2L, "upload2", savedProduct);
 
 
-        imageRepository.save(image1);
-        imageRepository.save(image2);
+        Image savedImage1 = imageRepository.save(image1);
+        Image savedImage2 = imageRepository.save(image2);
 
 
         //when
-        List<Image> images = productRepository.findImagesByProductId(FileType.PRODUCT_IMAGE, savedProduct.getId());
+
+
+        List<Image> images= imageRepository.findImagesByTargetIdAndFileType(FileType.PRODUCT_IMAGE, savedProduct.getId());
+
 
         //then
         assertThat(images).extracting(Image::getFileType, Image::getId, Image::getUploadName)
                 .contains(
-                        tuple(FileType.PRODUCT_IMAGE, 1L, "upload1"),
-                        tuple(FileType.PRODUCT_IMAGE, 2L, "upload2")
+                        tuple(FileType.PRODUCT_IMAGE, savedImage1.getId(), "upload1"),
+                        tuple(FileType.PRODUCT_IMAGE, savedImage2.getId(), "upload2")
                 );
     }
 
@@ -92,7 +102,7 @@ class ImageRepositoryTest {
     }
     private static Image createImage(long id, String upload, Product savedProduct) {
         Image image = Image.builder()
-                .id(id)
+//                .id(id)
                 .fileType(FileType.PRODUCT_IMAGE)
                 .uploadName(upload)
                 .targetId(savedProduct.getId())
