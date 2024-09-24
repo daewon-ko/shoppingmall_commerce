e043391814b04e62a7b5bb74426b89d3a4cc5010
pinetree <eodnjs568@gmail.com>
2024-09-24 16:17:35 +0900

    Feat : ProductSearchCondition, ProductQueryResponseDto 작성
    - 검색 조건, 조회 시 응답 DTO 작성

---
+++ shoppingmall/src/main/java/shppingmall/commerce/product/dto/response/ProductQueryResponseDto.java
@@ -0,0 +1,42 @@
+package shppingmall.commerce.product.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import lombok.Getter;
+import shppingmall.commerce.image.dto.response.ImageResponseDto;
+import shppingmall.commerce.product.entity.Product;
+
+import java.util.List;
+
+@Getter
+public class ProductQueryResponseDto {
+    private Long id;
+    private String name;
+    private int price;
+    private Long categoryId;
+    private String categoryName;
+    private List<ImageResponseDto> imageResponseDtos;
+
+    @Builder
+    @QueryProjection
+    public ProductQueryResponseDto(Long id, String name, int price, Long categoryId, String categoryName, List<ImageResponseDto> imageResponseDtos) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.imageResponseDtos = imageResponseDtos;
+    }
+
+
+    public static ProductQueryResponseDto of(Product product, List<ImageResponseDto> imageResponseDtos) {
+        return ProductQueryResponseDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .categoryId(product.getCategory().getId())
+                .categoryName(product.getCategory().getName())
+                .imageResponseDtos(imageResponseDtos)
+                .build();
+    }
+}
