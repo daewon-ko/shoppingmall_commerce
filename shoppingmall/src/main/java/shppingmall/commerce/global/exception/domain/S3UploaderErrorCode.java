2ff458789873abf1b18689b592916936c02e59b4
pinetree <eodnjs568@gmail.com>
2024-11-20 14:08:47 +0900

    Feat : S3Uploader 구현
    - 간단하게 S3Upload만 기능 구현
    - 이미지 조회, 삭제 등은 추후 구현예정

---
+++ shoppingmall/src/main/java/shppingmall/commerce/global/exception/domain/S3UploaderErrorCode.java
@@ -0,0 +1,20 @@
+package shppingmall.commerce.global.exception.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import shppingmall.commerce.global.exception.ErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum S3UploaderErrorCode implements ErrorCode {
+
+    INVALID_FILE_NAME(HttpStatus.BAD_REQUEST,"파일이름이 유효하지 않습니다."),
+    EMPTY_FILE(HttpStatus.BAD_REQUEST, "비어있는 파일입니다."),
+    UNEXPECTED_GET_URL_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 오류로 이미지 URL 조회를 실패했습니다.");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
