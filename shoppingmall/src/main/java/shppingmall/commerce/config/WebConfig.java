6ad1fa2e7e021f96443c94eb945643505ec8c280
pinetree <eodnjs568@gmail.com>
2024-11-21 15:46:03 +0900

    Chore : https 적용에 따른 Origin 추가

--- shoppingmall/src/main/java/shppingmall/commerce/config/WebConfig.java
+++ shoppingmall/src/main/java/shppingmall/commerce/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS gjdyd
-                .allowedOrigins("http://www.pinetree-toy.shop") // 허용출처 명기
+                .allowedOrigins("http://www.pinetree-toy.shop", "https://www.pinetree-toy.shop") // 허용출처 명기
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
