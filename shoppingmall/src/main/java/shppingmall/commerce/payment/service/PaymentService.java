67613ab3688ee83caf109165ad00b4b87b9c64c3
pinetree <eodnjs568@gmail.com>
2024-12-05 15:49:50 +0900

    Feat : Payment Controller / Service / Repository Layer 내 결제 요청, 조회 기능 작성

--- shoppingmall/src/main/java/shppingmall/commerce/payment/service/PaymentService.java
+++ shoppingmall/src/main/java/shppingmall/commerce/payment/service/PaymentService.java
@@ -1,39 +1,80 @@
 package shppingmall.commerce.payment.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shppingmall.commerce.global.exception.ApiException;
 import shppingmall.commerce.global.exception.domain.OrderErrorCode;
+import shppingmall.commerce.global.exception.domain.PaymentErrorCode;
 import shppingmall.commerce.order.entity.Order;
 import shppingmall.commerce.order.repository.OrderRepository;
-import shppingmall.commerce.payment.dto.PaymentConfirmRequest;
-import shppingmall.commerce.payment.dto.PaymentConfirmResponse;
+import shppingmall.commerce.payment.dto.PaymentResponse;
+import shppingmall.commerce.payment.dto.TossPaymentConfirmRequest;
+import shppingmall.commerce.payment.dto.TossPaymentConfirmResponse;
 import shppingmall.commerce.payment.entity.TossPayment;
 import shppingmall.commerce.payment.feign.PaymentClient;
 import shppingmall.commerce.payment.repository.PaymentRepository;
 
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PaymentService {
 
     private final PaymentClient paymentClient;
     private final OrderRepository orderRepository;
     private final PaymentRepository paymentRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final String REDIS_PAYMENT_PREFIX = "payment_order";
 
     //TODO : 결제에서의 트랜잭션 처리는 어떻게?
     // 외부 API를 연동하는데 트랜잭션 처리를 어떻게 해주는게 적절할까?
     @Transactional
-    public PaymentConfirmResponse confirm(final PaymentConfirmRequest request, final Long orderId) {
+    public PaymentResponse confirm(final TossPaymentConfirmRequest request, final Long orderId) {
         Order findOrder = orderRepository.findById(orderId).orElseThrow(() -> new ApiException(OrderErrorCode.NOT_EXIST_ORDER));
 
-        final PaymentConfirmResponse paymentConfirmResponse = paymentClient.confirmPayment(request);
-        TossPayment tossPayment = TossPayment.of(paymentConfirmResponse, findOrder);
+        // 외부 API 호출
+        final TossPaymentConfirmResponse tossPaymentConfirmResponse = paymentClient.confirmPayment(request);
+
+        // TossPayment 객체 생성 및 저장
+        TossPayment tossPayment = TossPayment.of(tossPaymentConfirmResponse, findOrder);
         paymentRepository.save(tossPayment);
 
-        return paymentConfirmResponse;
+        // Write-Through : Redis에 저장
+        String paymentKey = REDIS_PAYMENT_PREFIX + tossPayment.getOrder().getId();
 
+        redisTemplate.opsForValue().set(paymentKey, tossPayment);
+
+        // Cache Store 내에 하루 저장
+        // TODO : 결제 내역을 Cache에 얼마나 저장해야하는가?
+        redisTemplate.expire(paymentKey, Duration.ofDays(1));
+
+        PaymentResponse paymentResponse = PaymentResponse.from(tossPayment);
+
+        return paymentResponse;
+
+    }
+
+    public PaymentResponse getPayment(Long orderId) {
+        String redisKey = REDIS_PAYMENT_PREFIX + String.valueOf(orderId);// Read-Through 전략
+
+        // Read-Through 전략 -> 캐시에서 우선적으로 조회
+        TossPayment cachedPayment = (TossPayment) redisTemplate.opsForValue().get(redisKey);
+        if (cachedPayment != null) {
+            PaymentResponse paymentResponse = PaymentResponse.from(cachedPayment);
+            return paymentResponse;
+        }
+
+        // Cache Store에 없을 경우 DB에서 조회
+        TossPayment dbStoredTossPayment = paymentRepository.findByOrderId(orderId).orElseThrow(() -> new ApiException(PaymentErrorCode.NO_EXIST_PAYMENT));
+
+        // Cache Store에 저장
+        redisTemplate.opsForValue().set(redisKey, dbStoredTossPayment);
+        PaymentResponse paymentResponse = PaymentResponse.from(dbStoredTossPayment);
+        return paymentResponse;
     }
 
 
