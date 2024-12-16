1eeacbc3b51ffb5ccbbfb63127c94a69d2651bae
pinetree <eodnjs568@gmail.com>
2024-12-16 11:24:33 +0900

    Chore : infra 모듈 분리
    - Redis 관련 설정

--- shoppingmall/infra/src/main/java/shoppingmall/infra/config/redis/RedisConfig.java
+++ shoppingmall/infra/src/main/java/shoppingmall/infra/config/redis/RedisConfig.java
@@ -1,12 +1,12 @@
-package shppingmall.commerce.global.config.redis;
+package shoppingmall.infra.config.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
