b3fee0bba608487f0f06ecedb0f44f59a61fbbb5
pinetree <eodnjs568@gmail.com>
2025-01-02 14:16:11 +0900

    Feat: DomainService, Usecase 어노테이션 추가
    - Usecase 적용 위해 추가
    - Application Layer의 비즈니스로직과 도메인로직 구분 위해 추가

---
+++ shoppingmall/common/src/main/java/shoppingmall/common/annotation/DomainService.java
@@ -0,0 +1,12 @@
+package shoppingmall.common.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface DomainService {
+}
